c688623b0a69ef053873b533afd0c12e02c3904e
ebu3565 <mdebrahim3565@gmail.com>
2024-11-08 16:44:28 +0600

    mysql

--- login_page.php
+++ login_page.php
@@ -12,7 +12,7 @@ require_once 'includes/login_view.inc.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Comapatible" content="IE=edge">
     <title>Login Page</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
